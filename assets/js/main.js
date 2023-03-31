4285a461e49aa89e86ecc04c472452d61361980e
dannguyen1002 <dannguyen0973@gmail.com>
2023-03-31 15:29:30 +0700

    them footer

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const app = {
+    packages: [{
+        backgroundImage: './assets/img/container/destination-9.jpg',
+        Image: './assets/img/container/icons/arch.png',
+        name: 'Europe',
+        amount: 'items.length',
+        items: ['Berlin', 'Amsterdam', 'Tuscany'],
+    }],
+}
